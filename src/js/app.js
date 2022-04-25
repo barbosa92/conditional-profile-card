d79b51e40ad2346ebafffb164e7246836fa38259
barbosa92 <alexrp_92@hotmail.com>
2022-04-25 11:19:42 +0000

    RRSS and RRSS position need to be finished

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,38 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  // Si ambos (name y last) se han completado mostrará, de lo contrario si falta alguno mostrará Lucy Boilett
+  let fullname = `<h1>${variables.name} ${variables.lastname}</h1>`;
+  if (variables.name == null || variables.lastname == null)
+    fullname = "<h1>Lucy Boilett</h1>";
+
+  // Comprueba que role este definido
+  let job = `<h2>${variables.role}</h2>`;
+  if (variables.role == null) job = "<h2></h2>";
+
+  // Comprueba que city y country se hayan completado
+  let place = `<h2>${variables.city}, ${variables.country}<h2/>`;
+  if (variables.city == null || variables.country == null) place = "<h3></h3>";
+
+  // Comprueba que se añadan RRSS, de lo contrario asigna la URL de 4GeeksAcademy
+  let twit = `https://twitter.com/${variables.twitter}`
+  if (variables.twitter == null) twit = "<li><a href="https://twitter.com/4GeeksAcademy"><i class="fa-brands fa-twitter"></i></a></li>"
+  let git = `https://twitter.com/${variables.github}`
+  let link = `https://twitter.com/${variables.linkedin}`
+  let inst = `https://twitter.com/${variables.instagram}`
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+            ${fullname}
+            ${job}
+            ${place}
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa-brands fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa-brands fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa-brands fa-linkedin-in"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa-brands fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
